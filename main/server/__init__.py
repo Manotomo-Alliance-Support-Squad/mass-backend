9465024392f57ee46db144c8da4ea34313cd6cf0
Zapple <zizibaluba@gmail.com>
2020-11-03 22:01:54 -0800

    fix import problem

--- main/server/__init__.py
+++ main/server/__init__.py
@@ -6,9 +6,6 @@ from flask_caching import Cache
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
-from main.server import jwt
-from main.server.api import api_bp
-
 app = Flask(__name__, static_url_path='')
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 app_config = os.getenv("APP_CONFIG", "main.server.config.ProductionConfig")
@@ -21,6 +18,10 @@ db = SQLAlchemy(app)
 ma = Marshmallow()
 bcrypt = Bcrypt(app)
 
+# These need to be imported here probably due to some serial
+# runtime issue.
+from main.server import jwt  # noqa noreorder
+from main.server.api import api_bp  # noqa noreorder
 
 # Registering of the pai endpoint at "www.domain.xyz/api"
 app.register_blueprint(api_bp, url_prefix="/api")
