8671edfcb8b0cc412534ec274defe34387a75503
Zapple <zizibaluba@gmail.com>
2021-01-26 21:01:39 -0800

    change community message

--- frontend/src/components/headerSection/header.tsx
+++ frontend/src/components/headerSection/header.tsx
@@ -44,10 +44,10 @@ export default class HeaderSection extends Component<HeaderProps, HeaderState>
             <>
                 <header className="App-header">
                     <div className="community-message-card">
-                        <h1 className="community-message-header">A Community Message for Aloe</h1>
+                        <h1 className="community-message-header">For Everyone At Hololive</h1>
                         <div className="community-message-body">
-                            <p>Dear Aloe, thank you for everything. While it may have been but for a short while, we appreciate every little thing you've given us. We wish you the best of luck going forward.</p>
-                            <p>親愛なるアロエ様。短い間ではありましたが、一緒に過ごした時間に心より感謝しております。本当にありがとうございました。アロエ様のご健勝とご多幸をお祈り申し上げます。</p>
+                            <p>We have collected Valentine messages from your fans across the world. Thanks for all the entertainment all of you have provided, in the past, and yet to come.</p>
+                            <p>Happy Valentines day &lt;3</p>
                         </div>
                     </div>
                     <div style={{height: 50}}/>
