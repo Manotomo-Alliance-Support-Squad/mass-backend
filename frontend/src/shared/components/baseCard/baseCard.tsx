211b497f9ae64bac29ccdb990220a08b6567745d
tduong <duongt.sbc@gmail.com>
2021-02-10 22:09:34 -0800

    change title a little and also change colors

--- frontend/src/shared/components/baseCard/baseCard.tsx
+++ frontend/src/shared/components/baseCard/baseCard.tsx
@@ -9,8 +9,8 @@ import CardStyle2 from "../../../assets/cards/valentine_card2.svg";
 
 // TODO(#32): Change this to a class prop so inheriting classes can override it
 const CardStyleArr: Array<Array<string>> = [
-    [CardStyle1, "#f12169bf"],
-    [CardStyle2, "#f12169bf"],
+    [CardStyle1, "#f45787"],
+    [CardStyle2, "#f45787"],
 ]
 // TODO(#32): Remove when CardStyleArr is a prop, and exporting this value is no longer necessary
 export const CardStyleLength: number =
