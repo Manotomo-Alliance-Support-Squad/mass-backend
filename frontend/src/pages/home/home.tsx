f51e658e3a637a374f2cd5c7d131c5b1ff00c80e
Zapple <zizibaluba@gmail.com>
2021-02-18 08:52:12 -0800

    change contact to goog groups

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -125,7 +125,7 @@ export default class HomePage extends React.Component<HomePageProps, HomePageSta
                             <div className="notice-content">
                                 <p>These are all the messages we managed to collect! Happy Valentine's Day!</p>
                                 <p>これがすべての取集したメッセージですが！ハッピーバレンタイン！</p>
-                                <p style={{fontSize: 10}}>If you find any problems with the website, or if you would like to report a message, please contact Fishsauce#1324 on discord</p>
+                                <p style={{fontSize: 10}}>If you find any problems with the website, or if you would like to report a message, please contact us at manotomo@googlegroups.com</p>
                             </div>
                         </div>
                     </div>
