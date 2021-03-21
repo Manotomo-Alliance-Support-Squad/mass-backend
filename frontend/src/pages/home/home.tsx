3ee4ff518e66431388410b1e57271061b5702835
Zapple <zizibaluba@gmail.com>
2021-03-21 00:52:20 -0700

    add newlines to foreword

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -169,16 +169,16 @@ export default class HomePage extends React.Component<HomePageProps, HomePageSta
                                         <div className="justify-align-center notice-container" style={{"whiteSpace": "pre-line"}}>
                                             <MessageCard key={1} object={{ messageID: 0,
                                                 orig_msg: "はあと様へ\n\n\
-                                                私は今回の企画の主催者、シエウエイユエンと申します。\n\
-                                                メンバーシップの参加は僅かの一ヶ月けど、毎回あなたが真剣に配信を準備し、企画を考える姿を見る度に、自然にこの子を応援したい気持ちになる。気づいたら、もうこの子のために本気で泣いたり笑ったり、どうしよも無くあなたの事が好きになっちゃった。\n\
-                                                この企画の最初は、スタッフは本当に私一人しかないけど、みんながはあと様のためにだんだん集まって団結し、企画を大きくなりました！今の私はスペイン、ドイツと東アジアの人たちから助けてもらって、順調に企画を進んでいる。多分、人が何かのために、自分の全てを尽くし、真剣に努力すれば、周りだけじゃなく、世界中の人たちは必ずその情熱に応えて、支えてくれはず。\n\
-                                                きっと、Vtuberになる事も同じだと思います。この企画を参加する人たちはみんな、はあと様の努力に感動されて、応援しに来たのだ。頑張り屋さんのはあと様は、紛れもなくワールドワイドな最強アイドルです！\n\
+                                                私は今回の企画の主催者、シエウエイユエンと申します。\n\n\
+                                                メンバーシップの参加は僅かの一ヶ月けど、毎回あなたが真剣に配信を準備し、企画を考える姿を見る度に、自然にこの子を応援したい気持ちになる。気づいたら、もうこの子のために本気で泣いたり笑ったり、どうしよも無くあなたの事が好きになっちゃった。\n\n\
+                                                この企画の最初は、スタッフは本当に私一人しかないけど、みんながはあと様のためにだんだん集まって団結し、企画を大きくなりました！今の私はスペイン、ドイツと東アジアの人たちから助けてもらって、順調に企画を進んでいる。多分、人が何かのために、自分の全てを尽くし、真剣に努力すれば、周りだけじゃなく、世界中の人たちは必ずその情熱に応えて、支えてくれはず。\n\n\
+                                                きっと、Vtuberになる事も同じだと思います。この企画を参加する人たちはみんな、はあと様の努力に感動されて、応援しに来たのだ。頑張り屋さんのはあと様は、紛れもなくワールドワイドな最強アイドルです！\n\n\
                                                 この企画にはまだ第二段階があるので、是非お楽しみにしてください！はあと様、愛しています！",
                                                 tl_msg: "To Haato-sama,\n\n\
-                                                I am the initiator of this project, W.Y. Hsieh. \
-                                                Although I have been joining your membership for only a month, every time I see you fully engaged in streams and plans, I couldn’t help but feel encouraged by your strong passion. Without consciousness, I eventually started to cry for your touching moments and feel truly-warmed when you look happy. \
-                                                At the beginning, I was literally the only staff of this project. However, as time goes by, people started to gather in order to cheer you up and finally made this project become large. People from Spain, Germany and countries in east Asia have gave me countless helps so far. Probably people all around the world will gradually become willing to give you a hand once you show your passion and determination. \
-                                                I believe it is also the same as a Vtuber. All participants of this project are moved by Haato-sama’s great efforts and came to support you! As we all can tell, your persistence and hard work make you the world-wide strongest idol without doubt!\
+                                                I am the initiator of this project, W.Y. Hsieh.\n\n\
+                                                Although I have been joining your membership for only a month, every time I see you fully engaged in streams and plans, I couldn’t help but feel encouraged by your strong passion. Without consciousness, I eventually started to cry for your touching moments and feel truly-warmed when you look happy.\n\n\
+                                                At the beginning, I was literally the only staff of this project. However, as time goes by, people started to gather in order to cheer you up and finally made this project become large. People from Spain, Germany and countries in east Asia have gave me countless helps so far. Probably people all around the world will gradually become willing to give you a hand once you show your passion and determination.\n\n\
+                                                I believe it is also the same as a Vtuber. All participants of this project are moved by Haato-sama’s great efforts and came to support you! As we all can tell, your persistence and hard work make you the world-wide strongest idol without doubt!\n\n\
                                                 We have the second stage of our project so please look forward to it! We love you, Haato-sama!",
                                                 country: "TW", username: "Hsieh", }} cardStyleNum={1} language={language} />
                                         </div>
