d0bb575c4eb01fe678c5e374d49f2122a56103e6
Zapple <zizibaluba@gmail.com>
2021-04-10 19:20:55 -0700

    fix bug in api

--- backend/main/server/api.py
+++ backend/main/server/api.py
@@ -4,6 +4,7 @@ from flask_restful import Api
 from main.server.resources.Message import MessageListResource, MessageResource, MessageListRangeResource, MessageCount
 from main.server.resources.Game import GameCount, GameListResource
 from main.server.resources.Gallery import GalleryCount, GalleryListResource
+from main.server.resources.Video import VideoCount, VideoListResource
 from main.server.resources.Announcement import AnnouncementListResource, AnnouncementCount
 
 # Api Blueprint
@@ -27,8 +28,8 @@ api.add_resource(GalleryListResource, '/gallery')
 api.add_resource(GalleryCount, '/gallery/count')
 
 # Videos
-api.add_resource(GalleryListResource, '/video')
-api.add_resource(GalleryListResource, '/video/count')
+api.add_resource(VideoListResource, '/video')
+api.add_resource(VideoCount, '/video/count')
 
 # Announcements
 api.add_resource(AnnouncementListResource, '/announcements')
