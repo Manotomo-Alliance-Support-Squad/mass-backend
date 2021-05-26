db738b51a65f06f8a0241332b26ca1d7f45eba4f
unknown <vucuongJim@gmail.com>
2021-05-26 22:01:15 +0700

    Update header component

--- frontend/src/components/headerSection/header.tsx
+++ frontend/src/components/headerSection/header.tsx
@@ -6,31 +6,32 @@ interface HeaderState {
 }
 
 interface HeaderProps {
+
+}
+
+const headerMessage = {
+    messageTop: "We all miss your smiling presence and your voice that warm our hearts",
+    messageBottom: "Can't wait to have you back, we all love you so much, Akai Haato/Haachama",
 }
 
 
 export default class HeaderSection extends Component<HeaderProps, HeaderState>
 {
-    constructor(props: HeaderProps)
-    {
+    constructor(props: HeaderProps) {
         super(props);
     }
 
-    renderDefaultSection(): JSX.Element
-    {
+    render() {
+        const { messageTop, messageBottom } = headerMessage;
         return (
             <header className="app-header">
                 <div className="header-top">
-                    <p>We all miss your smiling presence and your voice that warm our hearts</p>
+                    <p>{messageTop}</p>
                 </div>
                 <div className="header-bottom">
-                    <p>Can't wait to have you back, we all love you so much, Akai Haato/Haachama</p>
+                    <p>{messageBottom}</p>
                 </div>
             </header>
-       )
-    }
-
-    render() {
-        return this.renderDefaultSection();
+        )
     }
 }
