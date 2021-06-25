0c8b7a7978dcd3923a37fb43c96bf815884d7066
unknown <vucuongJim@gmail.com>
2021-06-25 20:34:16 +0700

    use pathlib for getting placeholder image

--- main/utils/blur.py
+++ main/utils/blur.py
@@ -1,15 +1,26 @@
+from pathlib import Path, PurePath, PureWindowsPath
+from platform import system
 import blurhash
 import requests
 
+
 def getBlurCodeFromImage(imageUrl):
-        placeHolderImage = "./tmp/placeholder.jpg"
-        retrieveImageFromUrlAndSaveToLocal(imageUrl, placeHolderImage)
-        return blurhash.encode(placeHolderImage, x_components=4, y_components=6)
+    placeHolderImage = "/tmp/placeholder.jpg"
+    retrieveImageFromUrlAndSaveToLocal(imageUrl, placeHolderImage)
+    return blurhash.encode(placeHolderImage, x_components=4, y_components=6)
+
 
 def retrieveImageFromUrlAndSaveToLocal(imageUrl, localImageName):
-    headers = headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36'}
+    headers = headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Cafari/537.36'}
 
     pic = requests.get(imageUrl, headers=headers)
 
-    with open(localImageName, 'wb') as photo:
+    with Path(getFilePathByOperationSystem(localImageName)).open('wb') as photo:
         photo.write(pic.content)
+
+
+def getFilePathByOperationSystem(filePath):
+    if(system() == 'Windows'):
+        return PureWindowsPath('c:/Windows', filePath)
+    return PurePath(filePath)
